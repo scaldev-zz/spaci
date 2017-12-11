87600031473838984a8f7e01706b4b84cfba2012
Sophia Calderone <scalderone@sapient.com>
2017-12-11 00:41:00 -0500

    header sticky

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+const App = {
+
+	/**
+	 * Initialization
+	 */
+	init() {
+        this.isVisible();
+    },
+    isVisible(){
+        window.addEventListener("scroll", function() {
+          var elementTarget = document.getElementsByClassName('header')[0];
+          var height = elementTarget.offsetHeight;
+          if (window.pageYOffset > (elementTarget.offsetHeight)) {
+              elementTarget.classList.add('sticky');
+          }else {
+              elementTarget.classList.remove('sticky');
+          }
+        });
+    }
+};
+// TO-DO: don't have babel working yet
+// export default App;
+
+(function () {
+	App.init();
+})();
